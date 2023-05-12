144f2e5aa5956ace47598ecc15fff23305c31bc4
reuben <emavwoyareuben@gmail.com>
2023-05-11 21:01:11 -0400

    resolved merge conflict

--- index.js
+++ index.js
@@ -9,27 +9,20 @@ function getComputerChoice() {
   let options = ['rock', 'paper', 'scissors']
   return options[randomNumber];
 }
-
-/*playRound: A function that allows a player to play the Roock, paper, scissors game with the computer
-
-PlayerSelection: The input that will be recieved from the player.
-
-ComputerSelection: A randomnly generated input by the computer, that will be received from the computer.
-
-return: Returns either Player (player wins), Computer (computer wins) or Draw (draw game)
-
-
-
-*/
-
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(userSelection, computerSelection) {
+  
   let result;
   let playerSelection = userSelection.toLowerCase()
   if((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')){
+    computerScore += 1;
+   
    result = `${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${userSelection}. You loose!`
   }
   else if((computerSelection === "rock" && playerSelection === "paper") || (computerSelection === "paper" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "rock")){
+    playerScore += 1;
     result = `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}. You WIN`
   }
   else if(playerSelection === computerSelection){
@@ -41,35 +34,26 @@ function playRound(userSelection, computerSelection) {
   return result;
 }
 
-/*
-game : A function that plays the Rock, paper scissors game, over a given number of times. (5 in this case) and keeps a score of the winner of each round.
 
-return: Returns the player with the highest score or a draw
-*/
-
-function game() {
-  let gameResponse,
-    computerScores = 0,
-    playerScores = 0,
-    highestScore;
-  for (let i = 1; i <= 5; i++) {
-    let userSelection = prompt("Enter either Rock, paper or scissors")
-    gameResponse = playRound(userSelection, getComputerChoice());
-    console.log(gameResponse)
-
-    if (gameResponse.includes('You loose!')) {
-      
-      computerScores++;
-    } else if (gameResponse.includes('You WIN')) {
-      playerScores++;
-    }
+let buttons = document.querySelectorAll('button')
+let resultp = document.querySelector('p')
+let resultDiv = document.getElementById('resultdiv')
+buttons.forEach(button =>{
+  button.addEventListener('click', function gameStarts(){ 
+    let gamePlay = playRound(button.textContent, getComputerChoice())
+    resultDiv.classList.add('resultdiv')
+    resultp.innerHTML = gamePlay + `<br><br>` +  `Your current score = ${playerScore}` + "<br><br>" + `Computer current score = ${computerScore}`
+  if(playerScore > computerScore && playerScore >= 5){
+    resultp.innerHTML = gamePlay + `<br><br>` +  `Your current score = ${playerScore}` + "<br><br>" + `Computer current score = ${computerScore}` + "<br><br>" +
+    `Congratulations you WON with a total score of ${playerScore - computerScore}` + "<br><br>" + "Reload the page to play the game again" 
+    buttons.forEach(butt => butt.disabled = true)
   }
-  if (computerScores > playerScores) {
-    highestScore = `Computer WON with a total score of = ${computerScores}\n VS your score of = ${playerScores}`;
-  } else if (playerScores > computerScores)
-    highestScore = `You WON with a total score of ${playerScores}. VS computer score of ${computerScores}`;
-  else {
-    highestScore = `Computer score = ${computerScores} VS your score = ${playerScores} NO WINNER`;
-  }
-  return highestScore;
-}
+  else if(computerScore > playerScore && computerScore >= 5){
+    resultp.innerHTML = gamePlay +  `<br><br>` +  `Your current score = ${playerScore}` + "<br><br>" + `Computer current score = ${computerScore}` + "<br><br>" +
+    `Better luck next time. Computer WON with a total score of ${computerScore - playerScore}` + "<br><br>" + "Reload the page to play the game again"
+    buttons.forEach(butt =>  
+      butt.disabled = true)
+  }  
+  })
+})
+
